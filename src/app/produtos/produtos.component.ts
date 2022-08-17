ac91b8fa5fc7dde78933ba14f817dc596ac7cde8
Ádria Aline Cortes <adria.aline@gmail.com>
2022-08-17 16:08:42 -0300

    Criando funcionalidade da barra de pesquisa de produtos

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IProduto} from '../produtos';
 import { ProdutosService } from '../produtos.service';
 
@@ -12,11 +13,22 @@ export class ProdutosComponent implements OnInit {
   produtos: IProduto[] | undefined; //CRIANDO VETOR COM OS DADOS DO ARQUIVO PRODUTOS.TS
   
   constructor(
-    private produtosService: ProdutosService //pegando dados pelo serviço! produtos.service
+    private produtosService: ProdutosService, //pegando dados pelo serviço! produtos.service
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.produtos =this.produtosService.getAll();
+    const produtos = this.produtosService.getAll();
+    this.route.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLowerCase();
+    //quanto tiver uma mudança nos parametros ele tras os paramentros ! pegando valor da descricao! LowerCase(maiusculo).
+        if(descricao)
+        {
+            this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+            return;
+        }
+        this.produtos=produtos;
+    } )
   }
 
 }
